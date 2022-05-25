da7bcfa3f4e4de40ae61dca9cb9a3d12131e8652
Akilesh Rao <akilesh.rao@routemobile.com>
2022-05-25 12:03:56 +0530

    Moved stuff from instagram-clone-1 to root

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,13 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const num = 10;
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+        <p>Hello Vite + React! {num}</p>
         <p>
           <button type="button" onClick={() => setCount((count) => count + 1)}>
             count is: {count}
